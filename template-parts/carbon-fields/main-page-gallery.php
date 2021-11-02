7d932491c2cb58158e505a1125c3edc3dabed34c
Artur8800 <artursviridonov@gmail.com>
2021-11-03 00:42:01 +0200

    Fix issue \#2

--- template-parts/carbon-fields/main-page-gallery.php
+++ template-parts/carbon-fields/main-page-gallery.php
@@ -6,6 +6,18 @@ use Carbon_Fields\Field;
 add_action('carbon_fields_register_fields', 'crb_add_gallery');
     function crb_add_gallery()
     {
+
+        $menu_list = array();
+
+        if( $menu_items = wp_get_nav_menu_items('menu-1') ) { 
+                        
+            foreach ( (array) $menu_items as $key => $menu_item ) {
+                $id = $menu_item->ID;
+                $title = $menu_item->title;
+                $menu_list[$id] = $title;
+            }          
+        }
+
 Block::make( __( 'My Shiny Gutenberg Block' ) )
 	->add_fields( array(
         Field::make( 'complex', 'crb_media_item' )
@@ -27,17 +39,40 @@ Block::make( __( 'My Shiny Gutenberg Block' ) )
                 ->set_duplicates_allowed( true )
                 ->set_width( 50 )
                 ->set_type( array( 'image', 'crb_image' ) )
-            ) )
+            ) ),
+        Field::make('select', 'crb_gallery_button', __('Select a section for a link'))
+            ->set_options($menu_list),
+        Field::make('text', 'crb_gallery_header', __('Gallery heading text'))
+            ->set_default_value( __('Gallery heading text') ),
            
 	))
     ->set_inner_blocks( true )
     ->set_render_callback( function ( $fields, $attributes, $inner_blocks ) {
 		?>
 
+<?php                   $post_count = $fields['crb_gallery_button'];
+                        $link = get_home_url();
+                        $button_text;
+                        if( $menu_items = wp_get_nav_menu_items('menu-1') ) { 
+                            foreach ( (array) $menu_items as $key => $menu_item ) {
+                                $menu_id = $menu_item->ID;
+                                $menu_url = $menu_item->url;
+                                $menu_text = $menu_item->title;
+                                    if($menu_id == $post_count) {
+                                        $link = $menu_url;
+                                        $button_text = $menu_text;
+                                    }
+                            }          
+                        } ?>
 <section class="gallery section">
     <!-- fix section name-->
     <div class="heading__block">
-        <h2 class="main-title">Новини</h2>
+        <?php
+       
+       if (!empty($fields['crb_gallery_header']) ) {
+           echo '<h2 class="main-title">' . esc_html($fields['crb_gallery_header']) . '</h2>'; 
+       } 
+   ?>
     </div>
     <div id="imageGallery" class="swiper__container">
         <div class="swiper__row swiper__gallery">
@@ -122,9 +157,14 @@ Block::make( __( 'My Shiny Gutenberg Block' ) )
             </div>
         </div>
     </div>
-    <div class="button__wrapper">
-        <a href="" class="button__rounded button-blue">Бiльше</a>
-    </div>
+    <?php if (!empty($fields['crb_gallery_button'])) {
+
+ 
+    $link_id = $fields['crb_gallery_button'];
+    $menu_items = wp_get_nav_menu_items($link_id);
+
+    echo '<div class="button__wrapper"><a class="button__rounded button-blue" href="' . esc_url($link) . '">' . esc_html($button_text) . '</a>'; 
+    }?>
 
 </section>
 
